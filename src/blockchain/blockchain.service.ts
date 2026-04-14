6bf012017768913aa1234752d2ab46232ded6616
Olowodarey <olowodarey8@gmail.com>
2026-04-14 13:21:28 +0100

    celo contract

--- src/blockchain/blockchain.service.ts
+++ src/blockchain/blockchain.service.ts
@@ -5,8 +5,8 @@ import { celo, base } from 'viem/chains';
 import { GIGIPAY_ABI } from './abi';
 
 export const CONTRACT_ADDRESSES: Record<number, Address> = {
-  [celo.id]: '0x7B7750Fb5f0ce9C908fCc0674F8B35782F6d40B3',
-  [base.id]: '0xEdc6abb2f1A25A191dAf8B648c1A3686EfFE6Dd6',
+  [celo.id]: '0x70b92a67F391F674aFFfCE3Dd7EB3d99e1f1E9a8', // Celo mainnet
+  [base.id]: '0xEdc6abb2f1A25A191dAf8B648c1A3686EfFE6Dd6', // Base mainnet
 };
 
 @Injectable()
